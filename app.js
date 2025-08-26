98d1f9329573bd42d70d68d0d43850d24ce1c397
shakikk <shakik03@gmail.com>
2025-08-26 02:31:48 +0100

    Completed fibs and mergeSort algorithm

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function fibs(num){
     return fibArray;
 }
 
+//Recursive Method
 function fibsRec(num){
     if (num == 2) {
         return [0, 1];
@@ -33,3 +34,32 @@ function fibsRec(num){
 console.log(fibs(8));
 
 console.log(fibsRec(8));
+
+function mergeSort(arr){
+    if(arr.length<2){
+        return arr;
+    }
+    let mid = Math.ceil(arr.length/2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid, arr.length);
+    
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let arr = [];
+    let i = 0, j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            arr.push(left[i]);
+            i++;
+        } else {
+            arr.push(right[j]);
+            j++;
+        }
+    }
+    return arr.concat(left.slice(i), right.slice(j));
+}
+
+console.log(mergeSort([4,6,66,3,2,4,8]));
